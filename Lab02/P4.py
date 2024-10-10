045cc82c3e24870657b03c1969c194f4149bb149
Mareantz <marianciotir@gmail.com>
2024-10-10 10:34:36 +0300

    fixed 4th problem

--- Lab02/P4.py
+++ Lab02/P4.py
@@ -1,4 +1,10 @@
 #Write a script that converts a string of characters written in UpperCamelCase into lowercase_with_underscores.
 
-def lowercase_underscore(s:str) ->str:
-    return s.lower().replace(" ","_")
+def lowercase_underscore(s: str) -> str:
+    result = ""
+    for char in s:
+        if char.isupper() and result:
+            result += "_" + char.lower()
+        else:
+            result += char.lower()
+    return result
