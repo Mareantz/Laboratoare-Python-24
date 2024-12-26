d0a67e37cf0f79b23403848cfa1d07689d08c2c3
Mareantz <marianciotir@gmail.com>
2024-12-26 14:22:28 +0200

    fixed the highlight of potential moves

--- BackgammonProject/main.py
+++ BackgammonProject/main.py
@@ -5,38 +5,51 @@ COLOR_2 = "#8B4513"  # maro
 COLOR_3 = "#D8B98A"  # bej (fundal)
 
 
-def get_board_state():
+def get_board_state(is_white_home_right=True):
     triangles = [Triangle(i) for i in range(24)]
-
-    triangles[0].pieces_black = 2
-    triangles[5].pieces_white = 5
-    triangles[7].pieces_white = 3
-    triangles[11].pieces_black = 5
-    triangles[12].pieces_white = 5
-    triangles[16].pieces_black = 3
-    triangles[18].pieces_black = 5
-    triangles[23].pieces_white = 2
+    pieces = {
+        "white_home_right": {
+            0: {"black": 2}, 5: {"white": 5}, 7: {"white": 3}, 11: {"black": 5},
+            12: {"white": 5}, 16: {"black": 3}, 18: {"black": 5}, 23: {"white": 2}
+        },
+        "black_home_left": {
+            0: {"white": 2}, 5: {"black": 5}, 7: {"black": 3}, 11: {"white": 5},
+            12: {"black": 5}, 16: {"white": 3}, 18: {"white": 5}, 23: {"black": 2}
+        }
+    }
+    config = pieces["white_home_right"] if is_white_home_right else pieces["black_home_left"]
+    for idx, piece_data in config.items():
+        triangles[idx].pieces_white = piece_data.get("white", 0)
+        triangles[idx].pieces_black = piece_data.get("black", 0)
 
     return triangles
 
 
-def get_triangle_coords(i, segment_width, W, H, top):
+def get_triangle_coords(i, segment_width, width, height, top):
     actual_col = i if i < 6 else (i + 1)
     if top:
-        x_left = W - actual_col * segment_width
+        x_left = width - actual_col * segment_width
         x_right = x_left - segment_width
-        return [x_left, 0, x_right, 0, (x_left + x_right) / 2, H / 2]
+        result = [x_left, 0, x_right, 0, (x_left + x_right) / 2, height / 2]
     else:
         x_left = actual_col * segment_width
         x_right = x_left + segment_width
-        return [x_left, H, x_right, H, (x_left + x_right) / 2, H / 2]
+        result = [x_left, height, x_right, height, (x_left + x_right) / 2, height / 2]
+    return result
+
+
+def calculate_target_index(start_index, distance):
+    target_index = start_index - distance
+    if 0 <= target_index < 24:
+        return target_index
+    return None
 
 
 class Triangle:
-    def __init__(self, index, pieces_white=0, pieces_black=0):
+    def __init__(self, index):
         self.index = index
-        self.pieces_white = pieces_white
-        self.pieces_black = pieces_black
+        self.pieces_white = 0
+        self.pieces_black = 0
         self.highlight_color = None
 
     def add_piece(self, color):
@@ -48,7 +61,7 @@ class Triangle:
     def remove_piece(self, color):
         if color == 'white' and self.pieces_white > 0:
             self.pieces_white -= 1
-        if color == 'black' and self.pieces_black > 0:
+        elif color == 'black' and self.pieces_black > 0:
             self.pieces_black -= 1
 
     def can_move(self, color):
@@ -60,12 +73,14 @@ class BackgammonBoard:
     def __init__(self, parent, is_white_home_right=True):
         self.parent = parent
         self.is_white_home_right = is_white_home_right
-        self.triangles = get_board_state()
+        self.triangles = get_board_state(is_white_home_right)
         self.dice = Dice()
+
         self.canvas = tk.Canvas(self.parent, bg=COLOR_3)
         self.canvas.pack(fill=tk.BOTH, expand=True)
         self.canvas.bind("<Configure>", self.on_resize)
         self.canvas.bind("<Button-1>", self.on_triangle_click)
+
         self.segment_width = None
         self.selected_triangle = None
         self.current_player_color = "white" if is_white_home_right else "black"
@@ -76,6 +91,14 @@ class BackgammonBoard:
         self.draw_board(event.width, event.height)
 
     def on_triangle_click(self, event):
+        """
+        white:
+          top => 12..23 left-> right => index=12+col
+          bottom => 11..0 => index=11-col
+        black:
+          top => 23..12 right-> left => index=23-col
+          bottom => 0..11 => index=col
+        """
         if not self.segment_width:
             return
 
@@ -94,147 +117,107 @@ class BackgammonBoard:
         else:
             index = 11 - col if self.is_white_home_right else col
 
-        print('Clicked on the triangle {0}'.format(index))
-
-        # Check if the triangle has the current player's pieces
+        print(f"Clicked on triangle {index}")
         triangle = self.triangles[index]
-        if (self.current_player_color == "white" and triangle.pieces_white > 0) or \
-                (self.current_player_color == "black" and triangle.pieces_black > 0):
-            self.selected_triangle = index  # Select this triangle
-            self.highlight_possible_moves(index)  # Highlight possible moves
+        has_pieces = (triangle.pieces_white > 0 if self.current_player_color == "white" else triangle.pieces_black > 0)
+        if has_pieces:
+            self.selected_triangle = index
+            self.highlight_possible_moves(index)
         else:
-            self.selected_triangle = None  # Deselect if invalid
+            self.selected_triangle = None
 
-        # Redraw the board
         self.canvas.delete("all")
         self.draw_board(self.canvas.winfo_width(), self.canvas.winfo_height())
 
     def highlight_possible_moves(self, start_index):
-        """
-        Highlight possible moves based on the current dice rolls.
-        """
-        self.reset_highlights()  # Clear previous highlights
-
-        moves = []
+        self.reset_highlights()
         dice_values = self.dice.rolls
+        possible_moves = dice_values + [sum(dice_values[:2])] if len(dice_values) > 1 else dice_values
 
-        for die in dice_values:
-            target_index = self.calculate_target_index(start_index, die)
-            if target_index is not None:
-                moves.append(target_index)
-
-        # Highlight combinations of dice rolls
-        if len(dice_values) > 1:
-            combined_roll = sum(dice_values[:2])
-            target_index = self.calculate_target_index(start_index, combined_roll)
-            if target_index is not None:
-                moves.append(target_index)
-
-        # Mark the possible moves
-        for move in moves:
-            if 0 <= move < 24:  # Ensure valid range
-                self.triangles[move].highlight_color = "green"
+        for move in possible_moves:
+            target_index = calculate_target_index(start_index, move)
+            if target_index is not None and self.triangles[target_index].can_move(self.current_player_color):
+                self.triangles[target_index].highlight_color = "green"
 
     def reset_highlights(self):
-        """
-        Reset all triangle highlights.
-        """
-        for triangle in self.triangles:
-            triangle.highlight_color = None
-
-
-    def calculate_target_index(self, start_index, distance):
-        target_index = start_index - distance  # White moves downward (23 to 0)
-        print(target_index)
-        if 0 <= target_index < 24:
-            return target_index
-        return None
+        for t in self.triangles:
+            t.highlight_color = None
 
     def roll_dice(self):
         self.dice.roll()
         self.canvas.delete("all")
         self.draw_board(self.canvas.winfo_width(), self.canvas.winfo_height())
 
-    def draw_board(self, width, height):
-        segment_width = width / 13.0
-        bar_left = 6 * segment_width
-        bar_right = 7 * segment_width
+    def get_triangle_index_and_top(self, idx):
+        if self.is_white_home_right:
+            if idx >= 12:
+                return 23 - idx, True
+            else:
+                return 11 - idx, False
+        else:
+            if idx >= 12:
+                return idx - 12, True
+            else:
+                return idx, False
 
+    def draw_board(self, width, height):
+        bar_left = 6 * self.segment_width
+        bar_right = 7 * self.segment_width
         self.canvas.create_rectangle(bar_left, 0, bar_right, height, fill="#444", outline="black")
         self.draw_triangles(width, height)
         self.draw_pieces(width, height)
         self.dice.draw(self.canvas, width, height)
 
     def draw_triangles(self, width, height):
-        self.segment_width = width / 13.0
+        for idx in range(24):
+            i, top = self.get_triangle_index_and_top(idx)
+            coords = get_triangle_coords(i, self.segment_width, width, height, top)
+            color = COLOR_1 if i % 2 == 0 else COLOR_2
+            self.canvas.create_polygon(*coords, fill=color, outline="black")
 
-        for i in range(12):
-            for top in [False, True]:
-                coords = get_triangle_coords(i, self.segment_width, width, height, top)
-                if self.triangles[i].highlight_color:
-                    color = self.triangles[i].highlight_color
-                else:
-                    color = COLOR_1 if (i % 2 == 0) else COLOR_2
-                self.canvas.create_polygon(*coords, fill=color, outline="black")
-                self.triangles[i].highlight_color = None  # Reset highlight color
+            if self.triangles[idx].highlight_color:
+                self.canvas.create_polygon(*coords, fill=self.triangles[idx].highlight_color, outline="black")
+                self.triangles[idx].highlight_color = None
 
     def draw_pieces(self, width, height):
-        segment_width = self.segment_width
-        checker_radius = max(5, int(min(width, height) / 40))
+        piece_radius = max(5, int(min(width, height) / 40))
+        distance_y = 2 * piece_radius
+
+        def draw_pieces_on_column(triangles, start_y, direction):
+            for i, triangle in enumerate(triangles):
+                white_pieces = triangle.pieces_white
+                black_pieces = triangle.pieces_black
+                if white_pieces == 0 and black_pieces == 0:
+                    continue
+                col = i if i < 6 else i + 1
+                x_center = (col + 0.5) * self.segment_width
+                for n in range(white_pieces):
+                    y = start_y + direction * n * distance_y
+                    self.draw_one_piece(x_center, y, "white", piece_radius)
+                for n in range(black_pieces):
+                    y = start_y + direction * (white_pieces + n) * distance_y
+                    self.draw_one_piece(x_center, y, "black", piece_radius)
 
         if self.is_white_home_right:
-            bottom_idx = list(range(0, 12))
-            top_idx = list(range(12, 24))
+            bottom_index = list(range(11, -1, -1))
+            top_index = list(range(12, 24))
         else:
-            bottom_idx = list(range(12, 24))
-            top_idx = list(range(0, 12))
+            bottom_index = list(range(0, 12))
+            top_index = list(range(23, 11, -1))
 
-        # Bottom
-        for i, triangle in enumerate(reversed([self.triangles[idx] for idx in bottom_idx])):
-            w_count, b_count = triangle.pieces_white, triangle.pieces_black
-            if w_count == 0 and b_count == 0:
-                continue
+        draw_pieces_on_column([self.triangles[x] for x in bottom_index], height - 20, -1)
+        draw_pieces_on_column([self.triangles[x] for x in top_index], 20, 1)
 
-            actual_col = i if i < 6 else (i + 1)
-            x_center = (actual_col + 0.5) * segment_width
-            start_y = height - 20
-            delta_y = 2 * checker_radius
-
-            for n in range(w_count):
-                y = start_y - n * delta_y
-                self.draw_one_piece(x_center, y, "white", checker_radius)
-            for n in range(b_count):
-                y = start_y - (w_count + n) * delta_y
-                self.draw_one_piece(x_center, y, "black", checker_radius)
-
-        # Upper
-        for i, triangle in enumerate([self.triangles[idx] for idx in top_idx]):
-            w_count, b_count = triangle.pieces_white, triangle.pieces_black
-            if w_count == 0 and b_count == 0:
-                continue
-
-            actual_col = i if i < 6 else (i + 1)
-            x_center = (actual_col + 0.5) * segment_width
-            start_y = 20
-            delta_y = 2 * checker_radius
-
-            for n in range(w_count):
-                y = start_y + n * delta_y
-                self.draw_one_piece(x_center, y, "white", checker_radius)
-            for n in range(b_count):
-                y = start_y + (w_count + n) * delta_y
-                self.draw_one_piece(x_center, y, "black", checker_radius)
-
-    def draw_one_piece(self, x, y, color, r):
-        self.canvas.create_oval(x - r, y - r, x + r, y + r, fill=color, outline="black", width=2)
+    def draw_one_piece(self, x, y, color, piece_radius):
+        self.canvas.create_oval(x - piece_radius, y - piece_radius, x + piece_radius, y + piece_radius, fill=color,
+                                outline="black", width=2)
 
 
-def draw_dice_face(canvas, roll, x1, y1, x2, y2):
+def draw_dice_face(canvas, dice_value, x1, y1, x2, y2):
     center_x = (x1 + x2) / 2
     center_y = (y1 + y2) / 2
     radius = (x2 - x1) / 12
     spacing = 0.5
-
     offsets = {
         1: [(0, 0)],
         2: [(-spacing, -spacing), (spacing, spacing)],
@@ -244,11 +227,10 @@ def draw_dice_face(canvas, roll, x1, y1, x2, y2):
         6: [(-spacing, -spacing), (-spacing, 0), (-spacing, spacing), (spacing, -spacing), (spacing, 0),
             (spacing, spacing)],
     }
-
-    for dx, dy in offsets[roll]:
-        cx = center_x + dx * (x2 - x1) / 2
-        cy = center_y + dy * (y2 - y1) / 2
-        canvas.create_oval(cx - radius, cy - radius, cx + radius, cy + radius, fill="black")
+    for dx, dy in offsets[dice_value]:
+        px = center_x + dx * (x2 - x1) / 2
+        py = center_y + dy * (y2 - y1) / 2
+        canvas.create_oval(px - radius, py - radius, px + radius, py + radius, fill="black")
 
 
 class Dice:
@@ -265,7 +247,7 @@ class Dice:
             self.rolls = [d1, d2]
 
     def draw(self, canvas, width, height):
-        segment_width = width / 13.0
+        segment_width = width / 13
         bar_left = 6 * segment_width
         bar_right = 7 * segment_width
         bar_width = bar_right - bar_left
@@ -285,17 +267,18 @@ class Dice:
         start_x = center_x - total_width / 2
         start_y = center_y - total_height / 2
 
-        for i, roll in enumerate(self.rolls):
+        for i, roll_value in enumerate(self.rolls):
             row = i // 2
             col = i % 2
 
             x1 = start_x + col * (dice_size + dice_padding)
             y1 = start_y + row * (dice_size + dice_padding)
+
             x2 = x1 + dice_size
             y2 = y1 + dice_size
 
             canvas.create_rectangle(x1, y1, x2, y2, fill="white", outline="black", width=2)
-            draw_dice_face(canvas, roll, x1, y1, x2, y2)
+            draw_dice_face(canvas, roll_value, x1, y1, x2, y2)
 
 
 class MainMenu:
@@ -308,29 +291,27 @@ class MainMenu:
         self.menu_frame = tk.Frame(root, padx=20, pady=20)
         self.menu_frame.pack()
 
-        btn_comp = tk.Button(self.menu_frame, text="Play vs Computer", font=("Helvetica", 14),
-                             command=self.menu_vs_comp)
-        btn_comp.pack(pady=5)
+        button_ai = tk.Button(self.menu_frame, text="Play vs AI", font=("Helvetica", 14),
+                              command=self.menu_vs_ai)
+        button_ai.pack(pady=5)
 
-        btn_human = tk.Button(self.menu_frame, text="Play vs Human", font=("Helvetica", 14),
-                              command=self.menu_vs_human)
-        btn_human.pack(pady=5)
+        button_human = tk.Button(self.menu_frame, text="Play vs Human", font=("Helvetica", 14),
+                                 command=self.menu_vs_human)
+        button_human.pack(pady=5)
 
-    def menu_vs_comp(self):
+    def menu_vs_ai(self):
         self.menu_frame.destroy()
         self.sub_frame = tk.Frame(self.root, padx=20, pady=20)
         self.sub_frame.pack()
+        label = tk.Label(self.sub_frame, text="Choose your color:", font=("Helvetica", 14))
+        label.pack(pady=5)
 
-        lbl = tk.Label(self.sub_frame, text="Choose your color:", font=("Helvetica", 14))
-        lbl.pack(pady=5)
-
-        btn_white = tk.Button(self.sub_frame, text="White", font=("Helvetica", 14),
-                              command=lambda: self.start_board(is_white=True))
-        btn_white.pack(pady=5)
-
-        btn_black = tk.Button(self.sub_frame, text="Black", font=("Helvetica", 14),
-                              command=lambda: self.start_board(is_white=False))
-        btn_black.pack(pady=5)
+        button_white = tk.Button(self.sub_frame, text="White", font=("Helvetica", 14),
+                                 command=lambda: self.start_board(True))
+        button_white.pack(pady=5)
+        button_black = tk.Button(self.sub_frame, text="Black", font=("Helvetica", 14),
+                                 command=lambda: self.start_board(False))
+        button_black.pack(pady=5)
 
     def menu_vs_human(self):
         self.menu_frame.destroy()
@@ -342,14 +323,11 @@ class MainMenu:
         self.sub_frame.destroy()
         self.frame_board = tk.Frame(self.root)
         self.frame_board.pack(fill=tk.BOTH, expand=True)
-        if is_white:
-            self.board_app = BackgammonBoard(self.frame_board, is_white_home_right=True)
-        else:
-            self.board_app = BackgammonBoard(self.frame_board, is_white_home_right=False)
 
-        roll_button = tk.Button(self.frame_board, text="Roll Dice", command=self.board_app.roll_dice,
-                                font=("Helvetica", 14))
-        roll_button.pack(pady=5, side=tk.BOTTOM)
+        self.board_app = BackgammonBoard(self.frame_board, is_white_home_right=is_white)
+        button_roll = tk.Button(self.frame_board, text="Roll Dice", font=("Helvetica", 14),
+                                command=self.board_app.roll_dice)
+        button_roll.pack(pady=5, side=tk.BOTTOM)
 
 
 def main():
